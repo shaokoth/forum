b6d2c0ce77802531cdf3f4b05042d85d1c94b7d7
shaokoth <shadrackomondi005@gmail.com>
2025-01-29 15:00:54 +0300

    feat: Update home handler to parse home.html file

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
-// Define home handler function which writes a byte slice 
+
+// Define home handler function which writes a byte slice
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Web Forum"))
+	if r.URL.Path != "/" {
+		http.Error(w, "page not found", 404)
+	}
+	t, err := template.ParseFiles("templates/home.html")
+	if err != nil {
+		log.Fatal("error parsing html")
+	}
+	t.Execute(w, r)
 }
 
 func main() {
 	// Initialize a new servemux then register home function as handler for "/"
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-
 
 	// Start a new web server
 	// 8080 TCP network address to listen on
